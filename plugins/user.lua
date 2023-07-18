6dfce6deb4d93049b446b43d4aebbb2d5dff231b
ctorney <colin.j.torney@gmail.com>
2023-07-18 21:26:48 +0100

    wezterm

--- plugins/user.lua
+++ plugins/user.lua
@@ -49,42 +49,11 @@ return {
   {"jpalardy/vim-slime", 
     ft = {'python', 'lua', 'sh', 'zsh', 'bash', 'ipython'},
     config = function()
-      vim.g.slime_target = "neoterm"
-      vim.g.slime_default_config = {socket_name="default", target_pane="0"}
+      vim.g.slime_target = "wezterm"
+      vim.g.slime_default_config = {socket_name="default", pane_id="0"}
       vim.g.slime_dont_ask_default = 1
       vim.g.slime_bracketed_paste = 1
       vim.g.slime_no_mappings = 1
-      vim.cmd([[
-      nmap <leader>cv <Plug>SlimeConfig
-      nmap <leader>cc <Plug>SlimeSend1
-      nmap <leader>cn <Plug>SlimeSend2
-      nmap <leader>cp <Plug>SlimeSend3
-      nmap <leader>cb <Plug>SlimeSend4
-      nmap <leader>ca <Plug>SlimeSend5
-      nmap <leader>cd <Plug>SlimeSend6
-      nmap <leader>ce <Plug>SlimeSend7
-      nmap <leader>cf <Plug>SlimeSend8
-      nmap <leader>cg <Plug>SlimeSend9
-      nmap <leader>ch <Plug>SlimeSend10
-      nmap <leader>ci <Plug>SlimeSend11
-      nmap <leader>cj <Plug>SlimeSend12
-      nmap <leader>ck <Plug>SlimeSend13
-      nmap <leader>cl <Plug>SlimeSend14
-      nmap <leader>cm <Plug>SlimeSend15
-      nmap <leader>cn <Plug>SlimeSend16
-      nmap <leader>co <Plug>SlimeSend17
-      nmap <leader>cp <Plug>SlimeSend18
-      nmap <leader>cq <Plug>SlimeSend19
-      nmap <leader>cr <Plug>SlimeSend20
-      nmap <leader>cs <Plug>SlimeSend21
-      nmap <leader>ct <Plug>SlimeSend22
-      nmap <leader>cu <Plug>SlimeSend23
-      nmap <leader>cv <Plug>SlimeSend24
-      nmap <leader>cw <Plug>SlimeSend25
-      nmap <leader>cx <Plug>SlimeSend26
-      nmap <leader>cy <Plug>SlimeSend27
-      nmap <leader>cz <Plug>SlimeSend28
-      ]])
     end,
     
 
@@ -93,9 +62,9 @@ return {
   {
   'klafyvel/vim-slime-cells',
   requires = {{'jpalardy/vim-slime', opt=true}},
-  ft = {'ipython'},
+  ft = {'python','ipython'},
   config=function ()
-    vim.g.slime_target = "neoterm"
+    vim.g.slime_target = "wezterm"
     vim.g.slime_cell_delimiter = "^\\s*##"
     vim.g.slime_default_config = {socket_name="default", target_pane="0"}
     vim.g.slime_dont_ask_default = 1
