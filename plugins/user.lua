d9cba2dd9c423f64d8f6a8bd88dcd0825004f9e2
ctorney <colin.j.torney@gmail.com>
2024-01-09 10:24:40 +0000

    new yank plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -285,7 +285,7 @@ return {
       -- vim.g.slime_target = "tmux"
       -- vim.g.slime_config = {socket_name="default", target_pane="{right}"}
       vim.g.slime_target = "zellij"
-      vim.g.slime_config = {session_id="current", relative_pane="right", relative_move_back="left"}
+      vim.g.slime_config = {session_id="current", relative_pane="right"} --, relative_move_back="left"}
       vim.g.slime_dont_ask_default = 1
       vim.g.slime_bracketed_paste = 1
       vim.g.slime_no_mappings = 1
@@ -319,7 +319,36 @@ return {
         ]])
     end
   },
-
+  {
+    "gbprod/yanky.nvim",
+    lazy = false,
+    dependencies = {
+      { "kkharji/sqlite.lua" }
+    },
+    opts = {
+      ring = { storage = "sqlite" },
+    },
+    keys = {
+      { "<leader>p", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+      { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
+      { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before selection" },
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+      { "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+      { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+      { "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+      { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+      { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+      { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+      { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+      { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+      { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+      { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+    },
+  },
   {
     'lervag/vimtex',
     ft = {'tex'},
